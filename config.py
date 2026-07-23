17c3cd79c319b03dbf07ddd05171caa738405b35
VPepplow <pepplowvictor@gmail.com>
2026-07-22 21:25:25 -0300

    Arquivo configs
    Criando o arquivo configs e adicionando o caminho absoluto para o carregamento dos dados

---
+++ config.py
@@ -0,0 +1,9 @@
+# CAMINHOS
+# Colocando os caminhos que serão necessários usar ao longo do projeto
+from pathlib import Path
+
+# Pasta raiz do projeto
+ROOT = Path(__file__).resolve().parent
+
+# Pasta que os dados estão
+DATA_RAW = ROOT / 'data'
